16649722ae91c42672e6a956a668c7f1956fdda6
Dipak giri <dipakgiri1200@gmail.com>
2021-07-11 20:32:31 +0545

    Updates

---
+++ static/js/main.b0ba8c3d.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpe-dipak-store"]=this["webpackJsonpe-dipak-store"]||[]).push([[0],{11:function(e,t,n){"use strict";n.r(t);var r=n(1),c=n.n(r),o=n(3),i=n.n(o),s=(n(8),n(9),n(0));var d=function(){return Object(s.jsx)("div",{children:"Hello world!!"})};i.a.render(Object(s.jsx)(c.a.StrictMode,{children:Object(s.jsx)(d,{})}),document.getElementById("root"))},8:function(e,t,n){},9:function(e,t,n){}},[[11,1,2]]]);
+//# sourceMappingURL=main.b0ba8c3d.chunk.js.map
