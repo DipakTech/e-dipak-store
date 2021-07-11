56872c8917cf4ee9fd604015207867e6c603024d
Dipak giri <dipakgiri1200@gmail.com>
2021-07-11 21:36:13 +0545

    homepage style added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import HomePage from './homepage.component';
 
 function App() {
   return (
     <div >
-      Hello world!!
+      <HomePage/>
     </div>
   );
 }
---
+++ src/homepage.component.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import './homepage.styles.scss';
+const HomePage = () => (
+  <div className="homepage">
+    <div className="directory-menu">
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">Hats</h1>
+          <span className="title">Shop now</span>
+        </div>
+      </div>
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">banana</h1>
+          <span className="title">Shop now</span>
+        </div>
+      </div>
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">orange</h1>
+          <span className="title">Shop now</span>
+        </div>
+      </div>
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">mango</h1>
+          <span className="title">Shop now</span>
+        </div>
+      </div>
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">mango</h1>
+          <span className="title">Shop now</span>
+        </div>
+      </div>
+    </div>
+  </div>
+)
+export default HomePage;
